1cc00f1b6a7d6798b328c5f5207764dbf1daec97
sai920 <morsprincipiumest11@gmail.com>
2020-12-31 04:33:57 +0000

    [Finish]4-4

--- meshiterro/app/controllers/post_images_controller.rb
+++ meshiterro/app/controllers/post_images_controller.rb
@@ -2,6 +2,7 @@ class PostImagesController < ApplicationController
   
   def new
     @post_image = PostImage.new
+    @post_comment = PostComment.new
   end
 
   def create
@@ -12,11 +13,12 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page]).reverse_order
   end
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
   def destroy
